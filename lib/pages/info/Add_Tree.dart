dd7c84f6a54d905862443775a9bbfb6a5bf6366d
arjasairakesh213 <rakesh.arja@thejatech.com>
2023-11-16 16:44:23 +0530

    eb

--- lib/pages/info/Add_Tree.dart
+++ lib/pages/info/Add_Tree.dart
@@ -1,42 +1,138 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last, no_leading_underscores_for_local_identifiers, deprecated_member_use
+// ignore_for_file: non_constant_identifier_names, prefer_const_constructors, deprecated_member_use, sized_box_for_whitespace, camel_case_types, body_might_complete_normally_nullable, no_leading_underscores_for_local_identifiers, unused_local_variable
 
+import 'dart:convert';
+
+import 'package:eco/pages/Tree_LandingPage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AddTree extends StatefulWidget {
-  const AddTree({super.key});
+  final String? selectedState;
+
+  const AddTree({Key? key, this.selectedState}) : super(key: key);
 
   @override
-  State<AddTree> createState() => _dataState();
-
-  // void onDeviceAdded(String deviceName) {}
+  _AddTreeState createState() => _AddTreeState(selectedState: selectedState);
 }
 
-class _dataState extends State<AddTree> {
+class _AddTreeState extends State<AddTree> with SingleTickerProviderStateMixin {
+  String? selectedState;
+  String? selectedDistrict;
+
+  _AddTreeState({this.selectedState});
+  final _AgeOfTree = TextEditingController();
+  final _NumOfTree = TextEditingController();
+  final _KathaNumber = TextEditingController();
+  final _SurveyNumber = TextEditingController();
+  final _LandExtent = TextEditingController();
+  final _LandType = TextEditingController();
+  final _Height = TextEditingController();
+  final _SteamDiameter = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    final _lastnameController = TextEditingController();
-    final _firstnameController = TextEditingController();
-    final _pinController = TextEditingController();
-    final _emailController = TextEditingController();
-    final _subscriptioncontroller = TextEditingController();
-    final _zonecontroller = TextEditingController();
-    final _languagecontroller = TextEditingController();
-    final _mobileNocontroller = TextEditingController();
-    final _rolecontroller = TextEditingController();
-    final _address1controller = TextEditingController();
-    final _address2controller = TextEditingController();
-    final _address3controller = TextEditingController();
-    final _statecontroller = TextEditingController();
-    final _districtcontroller = TextEditingController();
-    final _citycontroller = TextEditingController();
-    final _landmarkcontroller = TextEditingController();
-
     return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () async {
+                  String AgeOfTree = _AgeOfTree.text;
+                  String NumOFTree = _NumOfTree.text;
+                  String KathaNUmber = _KathaNumber.text;
+                  String SurveyNumber = _SurveyNumber.text;
+                  String LandExtent = _LandExtent.text;
+                  String LandType = _LandType.text;
+                  String Height = _Height.text;
+                  String SteamDiameter = _SteamDiameter.text;
+
+                  SharedPreferences prefs =
+                      await SharedPreferences.getInstance();
+                  String? jwtToken = prefs.getString('jwt_token');
+
+                  if (jwtToken == null) {
+                    // Handle the case where the token is missing
+                    return;
+                  }
+
+                  // Prepare the request body to match the API's expected structure
+                  Map<String, dynamic> requestBody = {
+                    "ageOfTheTree": AgeOfTree,
+                    "numberOfTrees": NumOFTree,
+                    "kathaNumber": KathaNUmber,
+                    "surveyNumber": SurveyNumber,
+                    "landExtent": LandExtent,
+                    "landType": LandType,
+                    "height": Height,
+                    "steamDiameter": SteamDiameter
+                  };
+
+                  // Prepare the headers with the JWT token
+                  Map<String, String> headers = {
+                    'Authorization': 'Bearer $jwtToken',
+                    'Content-Type': 'application/json',
+                  };
+
+                  // Make the API POST request with headers and request body
+                  Uri apiUrl = Uri.parse('https://api.ecohex.in/tree/add');
+                  http
+                      .post(
+                    apiUrl,
+                    headers: headers,
+                    body: jsonEncode(requestBody),
+                  )
+                      .then((response) {
+                    if (response.statusCode == 200) {
+                      // Request was successful
+                      print('API request successful');
+
+                      // Parse the response if needed
+                      final responseJson = jsonDecode(response.body);
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => TreeLandinpPage(),
+                        ),
+                      );
+                      // Handle the response data as required
+                    } else {
+                      // Request failed
+                      print(
+                          'API request failed with status code: ${response.statusCode}');
+                      print('Response body: ${response.body}');
+                      // You can handle the error here
+                    }
+                  }).catchError((error) {
+                    // Request failed
+                    print('API request failed with error: $error');
+                    // You can handle the error here
+                  });
+                },
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.blue, fixedSize: Size(250, 70)
+                    // padding: EdgeInsets.symmetric(
+                    //     vertical: 10, horizontal: 20), // Adjust padding for size
+                    ),
+                child: Text('Add Tree',
+                    style: TextStyle(fontSize: 24, color: Colors.white)),
+              ),
+            ],
+          ),
+        ),
+      ),
       appBar: AppBar(
         iconTheme: IconThemeData(color: Colors.black),
         backgroundColor: Colors.white,
-        title: const Text(
-          "Add New Tree",
+        title: Text(
+          "Add_Tree",
           style: TextStyle(
               fontWeight: FontWeight.bold, color: Colors.black, fontSize: 25),
         ),
@@ -51,14 +147,16 @@ class _dataState extends State<AddTree> {
                 height: 20,
               ),
               Text(
-                'Age Of The Tree (years)',
+                'Age Of Trees(years)',
                 style: Theme.of(context).textTheme.headline6,
               ),
               TextField(
-                controller: _mobileNocontroller,
+                controller: _AgeOfTree,
                 decoration: InputDecoration(
-                    //hintText: 'Enter Mobile Number',
-                    ),
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+                  hintText: 'enter_Trees_Age',
+                ),
               ),
               SizedBox(
                 height: 20,
@@ -68,22 +166,26 @@ class _dataState extends State<AddTree> {
                 style: Theme.of(context).textTheme.headline6,
               ),
               TextField(
-                controller: _firstnameController,
+                controller: _NumOfTree,
                 decoration: InputDecoration(
-                    // hintText: 'Enter first name',
-                    ),
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+                  hintText: 'Enter_NumberOf_Trees',
+                ),
               ),
               SizedBox(
                 height: 20,
               ),
               Text(
-                'Katha Number',
+                'Katha number',
                 style: Theme.of(context).textTheme.headline6,
               ),
               TextField(
-                controller: _lastnameController,
+                controller: _KathaNumber,
                 decoration: InputDecoration(
-                  hintText: 'Enter Katha Number',
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+                  hintText: 'Enter Katha number',
                 ),
               ),
               SizedBox(
@@ -94,24 +196,13 @@ class _dataState extends State<AddTree> {
                 style: Theme.of(context).textTheme.headline6,
               ),
               TextField(
-                controller: _emailController,
+                controller: _SurveyNumber,
                 decoration: InputDecoration(
-                  hintText: 'Enter Survey Number',
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+                  hintText: 'Enter Survey number',
                 ),
               ),
-              SizedBox(
-                height: 20,
-              ),
-              // Text(
-              //   'Subscription validity Till',
-              //   style: Theme.of(context).textTheme.headline6,
-              // ),
-              // TextField(
-              //   controller: _subscriptioncontroller,
-              //   decoration: InputDecoration(
-              //     hintText: 'select validity',
-              //   ),
-              // ),
               SizedBox(
                 height: 20,
               ),
@@ -120,10 +211,12 @@ class _dataState extends State<AddTree> {
                 style: Theme.of(context).textTheme.headline6,
               ),
               TextField(
-                controller: _pinController,
+                controller: _LandExtent,
                 decoration: InputDecoration(
-                    //hintText: 'Enter Device Pin',
-                    ),
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+                  hintText: 'Enter Land Extent',
+                ),
               ),
               SizedBox(
                 height: 20,
@@ -133,8 +226,10 @@ class _dataState extends State<AddTree> {
                 style: Theme.of(context).textTheme.headline6,
               ),
               TextField(
-                controller: _rolecontroller,
+                controller: _LandType,
                 decoration: InputDecoration(
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 20, vertical: 0),
                   hintText: 'Enter Land Type',
                 ),
               ),
@@ -142,14 +237,16 @@ class _dataState extends State<AddTree> {
                 height: 20,
               ),
               Text(
-                'Height (meters)',
+                'Height(meters)',
                 style: Theme.of(context).textTheme.headline6,
               ),
               TextField(
-                controller: _languagecontroller,
+                controller: _Height,
                 decoration: InputDecoration(
-                    // hintText: 'Enter Preferd language',
-                    ),
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+                  hintText: 'Enter Height (meters)',
+                ),
               ),
               SizedBox(
                 height: 20,
@@ -159,32 +256,15 @@ class _dataState extends State<AddTree> {
                 style: Theme.of(context).textTheme.headline6,
               ),
               TextField(
-                controller: _rolecontroller,
+                controller: _SteamDiameter,
                 decoration: InputDecoration(
-                    // hintText: 'Enter prefered role',
-                    ),
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+                  hintText: 'Enter Steam Diameter (centi meters)',
+                ),
               ),
-              SizedBox(
-                height: 20,
-              ),
+              // ... (other text fields)
             ],
-          ),
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: SafeArea(
-          child: Center(
-            child: ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                primary: Colors.blue, // Change the button's background color
-                fixedSize: Size(500, 50), // Increase the button's size
-              ),
-              child: Text(
-                'Add Tree',
-                style: TextStyle(fontSize: 20, color: Colors.black),
-              ),
-            ),
           ),
         ),
       ),
